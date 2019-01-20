d3fbf455abe526c3240588736f49853e2912c3ae
Senthilkumar Shanmugam <sss.senthil@gmail.com>
2019-01-20 10:54:07 +0530

    Create Client_Registeration_details.sql

---
+++ Client_Registeration_details.sql
@@ -0,0 +1,22 @@
+drop table if exists oauth_client_details;
+create table oauth_client_details (
+  client_id VARCHAR(256) PRIMARY KEY,
+  resource_ids VARCHAR(256),
+  client_secret VARCHAR(256),
+  scope VARCHAR(256),
+  authorized_grant_types VARCHAR(256),
+  web_server_redirect_uri VARCHAR(256),
+  authorities VARCHAR(256),
+  access_token_validity INTEGER,
+  refresh_token_validity INTEGER,
+  additional_information VARCHAR(4096),
+  autoapprove VARCHAR(256)
+);
+
+-- insert client details
+INSERT INTO oauth_client_details 
+   (client_id, client_secret, scope, authorized_grant_types, 
+   authorities, access_token_validity, refresh_token_validity)
+VALUES
+   ('testClient', 'testSecret', 'read,write,trust', 'password,refresh_token', 
+   'ROLE_CLIENT,ROLE_TRUSTED_CLIENT', 900, 2592000);
